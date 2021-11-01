ab422367e050d4986b4704ebe33ac5a9ff48f296
Falk <gammalget@gmail.com>
2021-11-01 22:34:10 +0100

    crud moved to own module

---
+++ project/app/crud/users.py
@@ -0,0 +1,55 @@
+from typing import List, Optional, Union
+
+from app.schemas.user import (
+    UserPostPayloadSchema,
+    UserUpdatePayloadSchema,
+)
+from app.models.user import User
+from app.core import get_password_hash
+
+
+async def post(payload: UserPostPayloadSchema, hashed_password) -> int:
+
+    user = User(
+        username=payload.username,
+        email=payload.email,
+        full_name=payload.full_name,
+        disabled=payload.disabled,
+        hashed_password=hashed_password,
+    )
+    await user.save()
+
+    return user.id
+
+
+async def get(id: int) -> Optional[dict]:
+    user = await User.filter(id=id).first().values()
+    if user:
+        return user[0]
+    return None
+
+
+async def get_all() -> List:
+    users = await User.all().values()
+    return users
+
+
+async def delete(id: int) -> int:
+    user = await User.filter(id=id).first().delete()
+    return user
+
+
+async def put(id: int, payload: UserUpdatePayloadSchema) -> Union[dict, None]:
+    hashed_password = await get_password_hash(payload.plain_password.get_secret_value())
+
+    user = await User.filter(id=id).update(
+        username=payload.username,
+        email=payload.email,
+        full_name=payload.full_name,
+        disabled=payload.disabled,
+        hashed_password=hashed_password,
+    )
+    if user:
+        updated_user = await User.filter(id=id).first().values()
+        return updated_user[0]
+    return None
